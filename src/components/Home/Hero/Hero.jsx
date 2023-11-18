4382053c772410a904754aee0cc468b186ae8073
Daksh Maheshwari <dakshmaheshwari193@gmail.com>
2023-11-18 12:49:03 +0530

    home-hero-overlay-added

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import './Hero.css'
 export const Hero = () => {
   return (
     <div className="hero-section">
+      <div class="overlay"></div>
       <div className="hero-content">
         <h1>Welcome to DTU Physics Lab</h1>
         <p>Explore the world of physics with us.</p>
