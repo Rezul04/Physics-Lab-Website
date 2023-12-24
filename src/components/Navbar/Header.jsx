892284a2e8223426a9eebe484b1686d371238a4e
Daksh Maheshwari <dakshmaheshwari193@gmail.com>
2023-12-24 22:45:39 +0530

    1-Research themebox corrected 2-DTU logo updated on navbar and favicon

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import logo from '../../assets/DTUlogo.jpg'
+import logo from '../../assets/DTU-logo.png'
 import { Link } from "react-router-dom";
 
 export const Header = () => {
--- src/components/Research/ResearchThemes/Theme.jsx
+++ src/components/Research/ResearchThemes/Theme.jsx
@@ -5,24 +5,26 @@ import themeImage from '../../../assets/researchThemeImage.jpg'
 export const Theme = (theme) => {
   return (
     <div className="tMain">
-       
+
+      <div className="themeBlock">
         <div className="left">
-            <div className="themeBox">
+          <div className="themeBox">
             <div className="title1">
-            Theme title
+              Theme title
             </div>
             <div className="desc">
-            svnldskvv vjvj pov vpor kvpdr vpor pvrop vpor voprj vpojro vjewo fopew fpoew fpojw epfj wepofj powej fpowej fpojew pofjwe pofjwepo jfpowe jfpowej pofj wepofj powe jfpowej fpowj epof wepj fpwejfpjwepofjwep
+              svnldskvv vjvj pov vpor kvpdr vpor pvrop vpor voprj vpojro vjewo fopew fpoew fpojw epfj wepofj powej fpowej fpojew pofjwe pofjwepo jfpowe jfpowej pofj wepofj powe jfpowej fpowj epof wepj fpwejfpjwepofjwep
             </div>
             <button id="learnMore"><link rel="stylesheet" href="#" />Learn More</button>
-            </div>
+          </div>
         </div>
 
         <div className="right">
-            <div className="themeImage">
-              <img src={themeImage} alt=""  />
-            </div>
+          <div className="themeImage">
+            <img src={themeImage} alt="" />
+          </div>
         </div>
+      </div>
     </div>
   )
 }
