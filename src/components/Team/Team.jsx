664ed3348bf190b781b6d3b157aaf48330978bca
TeekshaHarish <102624456+TeekshaHarish@users.noreply.github.com>
2023-12-29 00:54:51 +0530

    Profile page

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { SubDept } from './SubDept/SubDept'
 import TeamMain from './TeamMain/TeamMain'
 import TeamHero from './TeamHero/TeamHero'
+import { Profile } from './Profile/Profile'
 
 export const Team = () => {
   return (
     <>
-    <TeamHero/>
+    <Profile/>
+    {/* <TeamHero/>
     <TeamMain/>
     <SubDept/>
     <SubDept/>
-    <SubDept/>
+    <SubDept/> */}
     </>
   )
 }
