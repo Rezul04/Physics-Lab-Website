ccefdc578c50f964c95ea523397d1f55486c53e8
siddharthgoel94 <siddharth_goel2105@outlook.com>
2024-01-01 03:52:01 +0530

    publication page fixed

--- src/components/Publications/PublicationsMain/PublicationsMain.jsx
+++ src/components/Publications/PublicationsMain/PublicationsMain.jsx
@@ -6,35 +6,48 @@ const PublicationsMain = () => {
     <div className='publicationMain'>
      <div class="list">
   <h2>Year wise publications</h2>
+  <div className="choiceHolder">
+    <button className="choice">Journal</button>
+    <button className="choice">Conference Paper</button>
+    <button className="choice">Book Chapters</button>
+    <button className="choice">All</button>
+  </div>
+ 
   <div className="yearBox">2023</div>
   {/* <div className="vBar"></div> */}
   <ul>
-    <li><span>Research on Semiconductors Aug 2023 by Prof ABC. <a>Read</a></span></li>
-    <li><span>Consectetur adipisicing elit</span></li>
-    <li><span>Ut labore et dolore magna aliqua</span></li>
-    <li><span>Ut enim ad minim veniam</span></li>
-    <li><span>Quis nostrud exercitation ullamco</span></li>
-    <li><span>Laboris nisi ut aliquip ex</span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    
   </ul>
   <div className="yearBox">2022</div>
   {/* <div className="vBar"></div> */}
   <ul>
-    <li><span>Research on Semiconductors Aug 2023 by Prof ABC. <a>Read</a></span></li>
-    <li><span>Consectetur adipisicing elit</span></li>
-    <li><span>Ut labore et dolore magna aliqua</span></li>
-    <li><span>Ut enim ad minim veniam</span></li>
-    <li><span>Quis nostrud exercitation ullamco</span></li>
-    <li><span>Laboris nisi ut aliquip ex</span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsum rerum accusamus maxime voluptatem expedita eaque perferendis eum veniam! Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    
   </ul>
   <div className="yearBox">2021</div>
   {/* <div className="vBar"></div> */}
   <ul>
-    <li><span>Research on Semiconductors Aug 2023 by Prof ABC. <a>Read</a></span></li>
-    <li><span>Consectetur adipisicing elit</span></li>
-    <li><span>Ut labore et dolore magna aliqua</span></li>
-    <li><span>Ut enim ad minim veniam</span></li>
-    <li><span>Quis nostrud exercitation ullamco</span></li>
-    <li><span>Laboris nisi ut aliquip ex</span></li>
+  <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
+    <li><span>Lorem ipsum dolor sit amet consectetur adipisicing elit., repellat consectetur adipisicing elit. Aug 2023 by Prof ABC. <a href='#'>Read</a></span></li>
   </ul>
 </div>
     </div>
